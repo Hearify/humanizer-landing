6088c9c4535689cea2be584e7d78dd2935bfe3d2
Yaarik16 <yarikdimbrovsky@gmail.com>
2024-11-07 19:58:04 -0600

    dev branch

--- src/templates/PageLayout/PageFooter/PageFooter.tsx
+++ src/templates/PageLayout/PageFooter/PageFooter.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 import styles from './PageFooter.module.scss';
 
 const PageFooter: React.FC = () => {
-  return (
-    <div className={styles.wrapper}>
-
-    </div>
-  );
+  return <div className={styles.wrapper}></div>;
 };
 
 export default PageFooter;
