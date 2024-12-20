32465c50e57e140b072b4012f471e28e560f7e1f
Yaarik16 <yarikdimbrovsky@gmail.com>
2024-12-20 11:31:59 -0600

    fix: page title and url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import { Nunito, Caveat } from 'next/font/google';
 
 import PageLayout from '@/templates/PageLayout/PageLayout';
@@ -26,15 +25,6 @@ if (typeof window === 'undefined') {
 
 /* eslint-disable react/jsx-props-no-spreading */
 const App = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter();
-
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL?.replace(/\/$/, '');
-  const cleanPath = router.asPath.split('#')[0].split('?')[0];
-  const canonicalEnUrl = `${baseUrl}${cleanPath === '/' ? '' : cleanPath}`;
-  const canonicalUkUrl = `${baseUrl}/uk${cleanPath === '/' ? '' : cleanPath}`;
-
-  const canonicalUrl = router.locale === 'uk' ? canonicalUkUrl : canonicalEnUrl;
-
   return (
     <>
       <Head>
@@ -48,8 +38,6 @@ const App = ({ Component, pageProps }: AppProps) => {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preload" href="/src/assets/images/humanize_bg.png" as="image/png" />
         <link rel="preload" href="/src/assets/images/contact_us.png" as="image/png" />
-
-        <link rel="canonical" href={canonicalUrl} />
 
         <meta name="robots" content="index, follow" />
       </Head>
