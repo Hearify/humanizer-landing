2be38bcb067428f81a22ee302ad12d648d86130a
Yaarik16 <yarikdimbrovsky@gmail.com>
2024-12-29 22:21:05 -0600

    feat: add why choose us section

--- src/templates/Home/Home.tsx
+++ src/templates/Home/Home.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import styles from './Home.module.scss';
 import Humanize from '@/templates/Home/Humanize/Humanize';
 import SeoContent from '@/templates/Home/SeoContent/SeoContent';
+import WhyChooseUs from '@/templates/Home/WhyChooseUs/WhyChooseUs';
 
 const Home: React.FC = () => {
   return (
     <main className={styles.wrapper}>
       <Humanize />
+      <WhyChooseUs />
       <SeoContent />
     </main>
   );
