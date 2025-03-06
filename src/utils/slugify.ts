4177c8bdc300797a9cdab9f88f54d52a679fc535
Ubuntu <azureuser@BiggerMachine.5pllfkb2j0ourdwwjad0h5ui1d.bx.internal.cloudapp.net>
2025-03-06 22:06:26 +0000

    eslint fix

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,7 +1,7 @@
 import slugifyLib from 'slugify';
 
 const slugify = (text: string): string => {
-  return slugifyLib(text, {
+  return (slugifyLib as (input: string, options: object) => string)(text, {
     lower: true,
     strict: true,
   });
